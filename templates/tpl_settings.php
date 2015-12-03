fd6fefbda2528fbc5d8c68a4809f8d2632f1cc31
Doc <hi@jamesdoc.com>
2015-12-03 20:41:53 +0000

    Remove templated header

--- templates/tpl_settings.php
+++ templates/tpl_settings.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-    <h2>WP Plugin Template</h2>
+    <h2>Sage Donate</h2>
     <form method="post" action="options.php">
         <?php @settings_fields('sd_sage_donate'); ?>
         <?php @do_settings_fields('sd_sage_donate'); ?>
@@ -115,8 +115,6 @@
                         </p> */ ?>
                     </td>
                 </tr>
-
-
 
             </table>
 
